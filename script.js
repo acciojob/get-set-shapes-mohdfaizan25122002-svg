fe90f69789b259a69d51519a7f2b46ad2f6dcbba
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-03-04 14:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // width and height same
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
-it("calculates area and perimeter correctly - 1", () => {
-  cy.visit(baseUrl + "/main.html");
-
-  cy.window().then((win) => {
-    const Rectangle = win.Rectangle;
-    const Square = win.Square;
-    cy.stub(win.console, "log").as("consoleLog");
-
-    const rect_w = 5;
-    const rect_h = 10;
-    const sq_s = 7;
-
-    const rectangle = new Rectangle(rect_w, rect_h);
-    const square = new Square(sq_s);
-
-    expect(rectangle.width).to.equal(rect_w);
-    expect(rectangle.height).to.equal(rect_h);
-    expect(rectangle.getArea()).to.equal(rect_w * rect_h);
-
-    expect(square.width).to.equal(sq_s);
-    expect(square.height).to.equal(sq_s);
-    expect(square.getArea()).to.equal(sq_s * sq_s);
-    expect(square.getPerimeter()).to.equal(sq_s * 4);
-  });
-});
 
